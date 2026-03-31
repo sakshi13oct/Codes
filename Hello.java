b8764f3f3819066a7d82860fbb8d9b5a80389a46
sakshi13oct <149258049+sakshi13oct@users.noreply.github.com>
2026-03-31 06:16:48 +0000

    modified:   Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Helloo world");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println("Helloo world" + "   " + n);
     }
 }
