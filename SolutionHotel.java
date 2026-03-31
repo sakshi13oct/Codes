045268462f0d920377ef2a086ddbb69ed77ce432
sakshi13oct <149258049+sakshi13oct@users.noreply.github.com>
2026-03-31 08:32:46 +0000

    modified:   SolutionHotel.java

--- SolutionHotel.java
+++ SolutionHotel.java
@@ -1,10 +1,76 @@
 import java.util.*;
 
+public class SolutionHotel{
+    public static int totalnoOfRoomsBooked(Hotel[] hotel, String cabFacility){
+        int ans = 0;
+        for(Hotel h: hotel) {
+            if(h.getCabFacility().equalsIgnoreCase(cabFacility) && h.getnoOfRoomsBooked() > 5) {
+                ans = ans + h.getnoOfRoomsBooked();
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Hotel[] h = new Hotel[4];
+        for(int i = 0;i<4;i++) {
+            int hotelId = sc.nextInt();
+            sc.nextLine();
+            String hotelName = sc.nextLine();
+            String dateOfBooking = sc.nextLine();
+            int noOfRoomsBooked = sc.nextInt();
+            sc.nextLine();
+            String cabFacility = sc.nextLine();
+            double totalBill = sc.nextDouble();
+
+            h[i] = new Hotel(hotelId, hotelName, dateOfBooking,noOfRoomsBooked,cabFacility, totalBill);
+        }
+        sc.nextLine();
+        String cabFac = sc.nextLine();
+
+        int ans = totalnoOfRoomsBooked(h, cabFac);
+        if(ans <= 0 ) {
+            System.out.print("No such rooms booked");
+        } else {
+            System.out.println(ans);
+        }
+    }
+}
+
 class Hotel {
     private int hotelId;
-    private string hotelName;
+    private String hotelName;
     private String dateOfBooking;
-    private int noOfroomsBooked;
+    private int noOfRoomsBooked;
     private String cabFacility;
     private double totalBill;
+
+    Hotel(int hotelId, String hotelName, String dateOfBooking, int noOfRoomsBooked, String cabFacility, double totalBill) {
+        this.hotelId = hotelId;
+        this.hotelName = hotelName;
+        this.dateOfBooking = dateOfBooking;
+        this.noOfRoomsBooked = noOfRoomsBooked;
+        this.cabFacility = cabFacility;
+        this.totalBill = totalBill;
+    }
+    public int getHotelId(){
+        return hotelId;
+    }
+    public String getHotelName() {
+        return hotelName;
+    }
+    public String getDateOfBooking(){
+        return dateOfBooking;
+    }
+    public int getnoOfRoomsBooked() {
+        return noOfRoomsBooked;
+    }
+    public String getCabFacility(){
+        return cabFacility;
+    }
+    public double getTotalBill(){
+        return totalBill;
+    }
+
 }
